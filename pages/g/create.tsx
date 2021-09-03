be20736fd0c45fcd9bfd9ab6d247fdb7236134bb
danzel-py <67574872+danzel-py@users.noreply.github.com>
2021-09-03 16:51:00 +0700

    added alerts for errors

--- pages/g/create.tsx
+++ pages/g/create.tsx
@@ -1,12 +1,18 @@
 import Layout from '../../components/Layout'
 import Link from 'next/link'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as Yup from 'yup'
 import { IFormCreateGroup } from '../../interfaces/index'
+import Router from 'next/router'
+import { string } from 'yup/lib/locale'
+
 
 export default function CreateGroup() {
+  const [message, setMessage] = useState<string>('')
+
+
   const validationSchema = Yup.object().shape({
     name: Yup.string()
       .required('Please specify group name')
@@ -48,9 +54,18 @@ export default function CreateGroup() {
       .catch((err) => {
         console.error(err)
       })
-    // this res is response
-
-
+      /*
+      * res.success, res.groupName on success
+      * res.message on failure
+      */
+     if(res.success){
+        Router.push(`/g/${res.groupName}`)
+     }
+     if(res.message){
+        console.log(res.message)
+        setMessage(res.message)
+     }
+    
     // redirect to g/something
   }
 
@@ -72,6 +87,20 @@ export default function CreateGroup() {
     <Layout>
       <div className="flex h-screen">
         <div className="w-full max-w-md m-auto">
+
+
+       {message && 
+
+         <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative my-3" role="alert">
+  <strong className="font-bold">Holy smokes!</strong><br/>
+  <span className="block sm:inline">{message}</span>
+  <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+    <svg onClick={()=>setMessage('')} className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+  </span>
+</div>
+      } 
+
+
           <form className="w-full max-w-md"
             onSubmit={handleSubmit(handleCreate)}
           >
@@ -128,7 +157,7 @@ export default function CreateGroup() {
                 <input className={` bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 ${errors.enterKey ? 'border-red-500  mb-3 ' : ''} `}
                   id="inline-enterKey"
                   name="enterKey"
-                  type="password"
+                  type="text"
                   placeholder="*******"
                   {...register('enterKey')}
                 />
@@ -144,7 +173,7 @@ export default function CreateGroup() {
               <div className="md:w-1/3"></div>
               <div className="md:w-2/3 flex items-center justify-between">
                 <button className="md:w-3/12 shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
-                  Join
+                  Create
               </button>
                 <div className="md:w-3/9"></div>
                 <div  className="md:w-3/9">
